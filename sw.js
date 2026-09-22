7b37d6f94a7ec01c195fdbfdd30a55a73d9f920a
Roberto Delgado Alcázar <rodalca03@gmail.com>
2026-09-22 18:51:19 +0200

    style: compactar toolbar superior en una sola linea y quitar botones de vista redundantes en movil

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* UniTask Service Worker v1.0 */
-const CACHE_NAME = 'unitask-cache-v1';
+/* UniTask Service Worker v1.1 */
+const CACHE_NAME = 'unitask-cache-v2';
 const STATIC_ASSETS = [
   './',
   './index.html',
